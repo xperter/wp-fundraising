f8b9adb7d5ae182aa71b3bd54bf358f0e216e78f
khalidjubair <khalidjubair09@gmail.com>
2018-07-20 11:58:52 +0600

    fix countdown timer

--- templates/loop/single/countdown-timer.php
+++ templates/loop/single/countdown-timer.php
@@ -2,7 +2,14 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 global $post;
-$end_date = get_post_meta( $post->ID, '_wf_duration_end', true );
+$xs_product = wc_get_product($post->ID);
+if($xs_product->get_type() == 'wf_donation'){
+    $end_date = get_post_meta( $post->ID, '_wfd_duration_end', true );
+}elseif($xs_product->get_type() == 'wp_fundraising'){
+    $end_date = get_post_meta( $post->ID, '_wf_duration_end', true );
+}else{
+    $end_date = '';
+}
 if (wf_get_option('_wf_hide_campaign_expiry_from_details', 'wf_basics')=='off'):
 ?>
 <div class="xs-single-sidebar xs-mb-40">
